6147dbc6451f5c787987ecd439a57facf6ff438b
bjarki <bjarkiste@gmail.com>
2021-09-17 10:37:44 +0000

    Azurecli config and dotfiles visible

--- windows/setup.sh
+++ windows/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+cp bashrc ~/.bashrc
+cp dircolors ~/.dircolors
+
 # vim
-mkdir ~/.vim ~/.vim/backup ~/.vim/tmp ~/.vim/undo
-cp vim/.vimrc ~/
+mkdir -p ~/.vim/backup ~/.vim/tmp ~/.vim/undo
+cp vim/vimrc ~/.vimrc
+cp vsvim/vsvimrc /mnt/c/Users/$USER/.vsvimrc
 
 # zsh
 sudo apt update && sudo apt install -y zsh
@@ -13,16 +17,20 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done"
 
-cp -f .aliases ~/
-cp -f .zpreztorc ~/
-cp -f .zshrc ~/
+cp -f aliases ~/.aliases
+cp -f zpreztorc ~/.zpreztorc
+cp -f zshrc ~/.zshrc
 
 chsh -s /bin/zsh
 
 # Docker
-mkdir ~/.docker
+mkdir -p ~/.docker
 cp docker/config.json ~/.docker
 
 # Windows Terminal
 cp windowsTerminal/settings.json /mnt/c/Users/$USER/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+
+# Azure cli
+mkdir -p /mnt/c/Users/$USER/.azure
+cp azurecli/config /mnt/c/Users/$USER/.azure/config
 
